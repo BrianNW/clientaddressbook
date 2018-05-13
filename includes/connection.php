422847825ed788eb00c0d10f7a8201119e65cbe9
Brian W <Briantechdom@gmail.com>
2018-05-13 09:55:55 -0700

    Completed db connection script

--- includes/connection.php
+++ includes/connection.php
@@ -8,4 +8,10 @@ $db             = "db_clientaddressbook";
 // create connection
 $conn = mysqli_connect();
 
+// check connection
+if (!$conn) {
+  // code...
+  die( "Connection failed: " . mysqli_connect_error() );
+}
+
  ?>

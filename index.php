c4aa8aacb434238e22944a7abee6b0dd95c54970
Brian W <Briantechdom@gmail.com>
2018-05-13 10:07:46 -0700

    Additional login script added

--- index.php
+++ index.php
@@ -4,9 +4,45 @@ session_start();
 
 if (isset( $_POST['login'] ) ) {
   // code...
+  $formUsername = $_POST['username'];
   $formEmail = $_POST['email'];
   $formPass = $_POST['password'];
+
+  // connect to database
+  include('includes/connection.php');
+
+  // create query
+  $query = "SELECT name, username, password FROM users WHERE email='$formEmail'";
+
+  // store the result
+  $result = mysqli_query($conn, $query);
+
+  // verify if result is returned
+  if( mysqli_num_rows($result) > 0 ) {
+
+    // store basic user data in variables
+    while( $row = mysqli_fetch_assoc($result) ) {
+      $username     = $row['username'];
+      $email        = $row['email'];
+      $password     = $row['password'];
+    }
+
+    verify hashed password with submitted password
+    if( password_verify( $formPass, $hashedPass ) ) {
+
+      // correct login details!
+      // store data in SESSION Variables
+      $_SESSION['loggedInUser'] = $name;
+
+      // redirects user to clients page
+      header( "Location: clients.php");
+
+    }
+
+  }
+
 }
+
 
 include('includes/header.php');
 
@@ -18,6 +54,10 @@ include('includes/header.php');
 <p class="lead">Log in to your account.</p>
 
 <form class="form-inline" action="clients.php" method="post">
+    <div class="form-group">
+        <label for="login-username" class="sr-only">Username</label>
+        <input type="text" class="form-control" id="login-username" placeholder="username" name="username">
+    </div>
     <div class="form-group">
         <label for="login-email" class="sr-only">Email</label>
         <input type="text" class="form-control" id="login-email" placeholder="email" name="email">
